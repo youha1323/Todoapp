39f28123e769e18f1511efb5c8758f32e66e627e
OkadaShotaro <b1016117@fun.ac.jp>
2019-05-21 20:50:51 +0900

    解説用のコメントを追加

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import ToDoList from './list';
 
-const list = [];
-ReactDOM.render(
-    <ToDoList toDoList={list}/>, 
+const list = []; // ここでtodoの情報を補完するための「配列」というものを宣言しているよ
+
+ReactDOM.render( // reactの機能を使って()内のものを画面上に表示させているよ
+    <ToDoList toDoList={list}/>, // 表示させるものとして、list.jsxで書いた「reactコンポーネント」というものを指定してるよ
+    　　　　　　　　　　　　　　　　// 更に、toDoList={list}という部分で、このreactコンポーネントにlistを渡しているよ
     document.getElementById('root')
 );
 
--- src/list.jsx
+++ src/list.jsx
@@ -1,50 +1,51 @@
 import React from 'react';
 
-class ToDoList extends React.Component{
-  constructor(props){
+// ファイル名についている.jsxは、JavaScript内でhtmlが記述できる中々優秀な奴だよ。気になったら調べてみよう。
+
+class ToDoList extends React.Component{ // 「reactコンポーネント」の機能を継承した、TodoListという名前の「class」を宣言しているよ
+  constructor(props){ // このように記述することで、index.jsで記述したlistを「props」という形で受け取ることができるよ
     super(props);
-    this.state = {
+    this.state = { // この「コンポーネント」で使用する「state」の初期値を宣言しているよ
       toDoList: props.toDoList,
       textTitle: '',
       textContent: ''
     };
   }　
 
-  addToDo(){
-    let list = this.state.toDoList;
-    list.push({ title: this.state.textTitle, content: this.state.textContent });
-    this.setState({toDoList: list});
+  addToDo(){ // 追加ボタンが押されたときの処理を書いている「関数」だよ
+    let list = this.state.toDoList; // この行の記述がわからなかったら、「代入」で調べてみよう！
+    list.push({ title: this.state.textTitle, content: this.state.textContent }); // 「push」は「配列」を操作するためのものだよ。調べてみよう！
+    this.setState({toDoList: list}); // 「setState」は「react」さんに備わった特殊能力だよ。調べてみよう！
   }
 
-  deleteToDo(i){
+  deleteToDo(i){ // 削除ボタンが押されたときの処理を書いている「関数」だよ
     let list = this.state.toDoList;
-    list.splice(i, 1);
+    list.splice(i, 1); // 「splice」は「配列」を操作するためのものだよ。調べてみよう！
     this.setState({toDoList: list});
   }
 
   render(){
-      console.log(this.state.toDoList)
-    const domList = this.state.toDoList.map((m, i) =>{
-      return <li key={i}>
-        タイトル：{m.title}<br/>
+    const domList = this.state.toDoList.map((m, i) =>{ // 「map」は「配列」を操作するためのものだよ。調べてみよう！(ここの記述が一番難しいかも...)                                                                                               
+      return <li key={i}> {/*<li>は「html」さんの特殊能力だよ。何をするものかわからなかったら調べてみよう */ }
+        タイトル：{m.title}<br/> {/*<br>も「html」さんの特殊能力だよ。 */}
         内容：{m.content}<br/>
-        <button onClick={e => this.deleteToDo(i)}>削除{i}</button>
+        <button onClick={e => this.deleteToDo(i)}>削除{i}</button> {/*<button>も「html」さんの特殊能力だよ。onClickという記述で、このボタンが押されたときの処理を追加できるよ*/}
       </li>;
     });
 
     return(
-      <div>
+    <div> {/*<div>も「html」さんの特殊能力なんじゃ */}
         <div>
 
           タイトル：<input type="text" value={this.state.textTitle} 
-            onChange={e => this.setState({textTitle: e.target.value})}/>
+            onChange={e => this.setState({textTitle: e.target.value})}/> {/*<input>も「html」さんの特殊能力だよ。type, value, onChangeは全部<input>さんの特殊能力だから、わからなかったら調べてみよう */}
 
           内容：<input type="text" value={this.state.textContent} 
             onChange={e => this.setState({textContent: e.target.value})}/>
 
         </div>
         <button onClick={e => this.addToDo()}>追加</button>
-        <ul>{domList}</ul>
+        <ul>{domList}</ul> {/*ここでさっき宣言したdomListを表示しているよ。あと<ul>も「html」さんの特殊能力なんじゃ */}
       </div>
     );
   }
